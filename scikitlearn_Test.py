7dd260e611c2c6915c0c31d8c8e88c8495cdd125
yuanyuanzero <yuan2-xu@rontech.co.jp>
2019-09-24 10:24:48 +0900

    First commit

---
+++ scikitlearn_Test.py
@@ -0,0 +1,10 @@
+# scikit-learn ライブラリの読み込み
+from sklearn import datasets
+
+#手書き文字セットを読み込む
+digits = datasets.load_digits()
+
+#どのようなデータか、確認してみる
+import matplotlib.pyplot as plt
+plt.matshow(digits.images[3], cmap="Greys")
+plt.show()
