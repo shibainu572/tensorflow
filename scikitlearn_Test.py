826caeb15a06bcf980c062f9f40eb11ffea73132
ks-han <ks-han@rontech.co.jp>
2019-09-24 11:04:26 +0900

    画像データとテストデータの準備

--- scikitlearn_Test.py
+++ scikitlearn_Test.py
@@ -1,12 +1,33 @@
 # scikit-learn ライブラリの読み込み
 from sklearn import datasets
 
-#手書き文字セットを読み込む
+# 手書き文字セットを読み込む
 digits = datasets.load_digits()
 
-#どのようなデータか、確認してみる
-import matplotlib.pyplot as plt
-plt.matshow(digits.images[3], cmap="Greys")
-plt.show()
+# 画像データとテストデータの準備
+# データセットには、「手書き数字の画像データ」と、それに対する「数字」が含まれます。
+# データを訓練データとテストデータに分け、訓練データで学習した結果を、テストデータで検証します。
 
-#結果として3の画像が表示される　
+# 画像データを配列にしたもの(numpy.ndarray型)
+x = digits.data
+
+# 画像データに対する数字(numpy.ndarray型)。ラベルと言う
+y = digits.target
+
+# 訓練データとテストデータに分ける
+# 訓練データ　: 愚数行
+x_train, y_train = x[0::2], y[0::2]
+# テストデータ : 奇数行
+x_test, y_test = x[1::2], y[1::2]
+
+#画像データ配列(愚数)
+print("x-train : ", x_train)
+
+#画像データに対する数字(愚数)
+print("y-train : ", y_train)
+
+#画像データ配列(奇数)
+print("x-test : ", x_test)
+
+#画像データに対する数字(奇数)
+print("y-test : ", y_test)
