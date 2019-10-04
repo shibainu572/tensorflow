9bc063243a5493224e76a8e6ae77e9b639e957f9
ks-han <ks-han@rontech.co.jp>
2019-10-04 16:59:54 +0900

    ファイルの内容に合わせて名前変更#

--- OpenCV/Intro_and_loading_Images_tutorial .py
+++ OpenCV/Intro_and_loading_Images_tutorial .py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread("watch.jpg", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('watch.jpg', cv2.IMREAD_GRAYSCALE)
 
-plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.imshow(img, cmap='gray', Interpolation='bicubic')
 plt.xticks([])
 plt.yticks([])
 plt.plot([200, 300, 400], [100, 200, 300], 'c', linewidth=5)
