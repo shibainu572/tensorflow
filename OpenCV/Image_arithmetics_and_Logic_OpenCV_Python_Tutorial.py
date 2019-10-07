2ea4c5e92cf21f1a310c3520642ab0202cc69467
ks-han <ks-han@rontech.co.jp>
2019-10-07 18:26:17 +0900

    イメージに新しいイメージを上書きするところまで実装(明日はソースの研究)

--- OpenCV/Image_arithmetics_and_Logic_OpenCV_Python_Tutorial.py
+++ OpenCV/Image_arithmetics_and_Logic_OpenCV_Python_Tutorial.py
@@ -1,22 +1,31 @@
 import cv2
 import numpy as np
 
-#500 x 250
+# Load two images
 img1 = cv2.imread('3D-Matplotlib.png')
-img2 = cv2.imread('mainsvmimage.png')
+img2 = cv2.imread('mainlogo.png')
 
-add = img1 + img2
-# void add(const Mat& src1, const Mat& src2, Mat& dst)
-# 2 つの配列同士，あるいは配列とスカラの 要素毎の和を求めます
-add2 = cv2.add(img1, img2)
-weighted = cv2.addWeighted(img1, 0.6, img2, 0.4, 0)
+# I want to put logo on top-left corner, So I create a ROI
+rows, cols, channels = img2.shape
+roi = img1[0:rows, 0:cols]
 
-print("add : ", add)
-print("cv2.add : ", add2)
-print("cv2.addWeighted : ", weighted)
+# Now create a mask of logo and create its inverse mask
+img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('add', add)
-cv2.imshow('cv2.add', add2)
-cv2.imshow('cv2.weighted', weighted)
+# add a threshold
+ret, mask = cv2.threshold(img2gray, 220, 255, cv2.THRESH_BINARY_INV)
+
+mask_inv = cv2.bitwise_not(mask)
+
+# Now black-out the area of logo in ROI
+img1_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+
+# Take only region of logo from logo image.
+img2_fg = cv2.bitwise_and(img2, img2, mask=mask)
+
+dst = cv2.add(img1_bg, img2_fg)
+img1[0:rows, 0:cols] = dst
+
+cv2.imshow('res', img1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
